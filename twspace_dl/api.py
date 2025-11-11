248b4feca5ebf132ddbe19078a9e813e4449c35b
Farzin Fadamin <ffadamin@gmail.com>
2025-11-11 13:39:55 -0500

    fix: user_tweets

--- twspace_dl/api.py
+++ twspace_dl/api.py
@@ -313,25 +313,37 @@ class GraphQLAPI(APIClient):
             return data["data"]["user_result_by_screen_name"]["result"]["rest_id"]
 
     def user_tweets(self, user_id, number_of_tweets):
-        query_id = "HuTx74BxAnezK1gWvYY7zg"
-        operation_name = "UserTweets"
-        variables = {
-            'userId': user_id,
-            'count': number_of_tweets,
-            "withTweetQuoteCount": True,
-            "includePromotedContent": True,
-            "withQuickPromoteEligibilityTweetFields": False,
-            "withSuperFollowsUserFields": True,
-            "withUserResults": True,
-            "withNftAvatar": False,
-            "withBirdwatchPivots": False,
-            "withReactionsMetadata": False,
-            "withReactionsPerspective": False,
-            "withSuperFollowsTweetFields": True,
-            "withVoice": True,
+        url = "https://x.com/i/api/graphql/oRJs8SLCRNRbQzuZG93_oA/UserTweets"
+        params = {
+            "variables": '{\"userId\":\"user_id_placeholder\",\"count\":count_placeholder,\"includePromotedContent\":true,\"withQuickPromoteEligibilityTweetFields\":true,\"withVoice\":true}',
+            "features": '{\"rweb_video_screen_enabled\":false,\"payments_enabled\":false,\"profile_label_improvements_pcf_label_in_post_enabled\":true,\"responsive_web_profile_redirect_enabled\":false,\"rweb_tipjar_consumption_enabled\":true,\"verified_phone_label_enabled\":false,\"creator_subscriptions_tweet_preview_api_enabled\":true,\"responsive_web_graphql_timeline_navigation_enabled\":true,\"responsive_web_graphql_skip_user_profile_image_extensions_enabled\":false,\"premium_content_api_read_enabled\":false,\"communities_web_enable_tweet_community_results_fetch\":true,\"c9s_tweet_anatomy_moderator_badge_enabled\":true,\"responsive_web_grok_analyze_button_fetch_trends_enabled\":false,\"responsive_web_grok_analyze_post_followups_enabled\":true,\"responsive_web_jetfuel_frame\":true,\"responsive_web_grok_share_attachment_enabled\":true,\"articles_preview_enabled\":true,\"responsive_web_edit_tweet_api_enabled\":true,\"graphql_is_translatable_rweb_tweet_is_translatable_enabled\":true,\"view_counts_everywhere_api_enabled\":true,\"longform_notetweets_consumption_enabled\":true,\"responsive_web_twitter_article_tweet_consumption_enabled\":true,\"tweet_awards_web_tipping_enabled\":false,\"responsive_web_grok_show_grok_translated_post\":false,\"responsive_web_grok_analysis_button_from_backend\":true,\"creator_subscriptions_quote_tweet_preview_enabled\":false,\"freedom_of_speech_not_reach_fetch_enabled\":true,\"standardized_nudges_misinfo\":true,\"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled\":true,\"longform_notetweets_rich_text_read_enabled\":true,\"longform_notetweets_inline_media_enabled\":true,\"responsive_web_grok_image_annotation_enabled\":true,\"responsive_web_grok_imagine_annotation_enabled\":true,\"responsive_web_grok_community_note_auto_translation_is_enabled\":false,\"responsive_web_enhance_cards_enabled\":false}',
+            "fieldToggles": '{\"withArticlePlainText\":false}'
         }
-        features = '{}'
-        return self.get(query_id, operation_name, variables, features)
+        params['variables'] = params['variables'].replace('user_id_placeholder', user_id).replace('count_placeholder', str(number_of_tweets))
+
+        headers = {
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Authorization": TWITTER_AUTHORIZATION,
+            "Content-Type": "application/json",
+            "Referer": f"https://x.com/{screen_name}",
+            "Sec-CH-UA": '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+            "Sec-CH-UA-Mobile": "?0",
+            "Sec-CH-UA-Platform": '"macOS"',
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+            "X-Twitter-Active-User": "yes",
+            "X-Twitter-Auth-Type": "OAuth2Session",
+            "X-Twitter-Client-Language": "en"
+        }
+
+        cookies = self.cookies
+        headers["X-CSRF-Token"] = cookies["ct0"]
+        response = requests.get(url, headers=headers, params=params, cookies=cookies)
+
+        return response.json()
 
     def user_by_id(self, user_id):
         query_id = "GazOglcBvgLigl3ywt6b3Q"
